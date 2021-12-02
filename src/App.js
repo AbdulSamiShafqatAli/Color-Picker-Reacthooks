36fe50dba5a21b15ec994fb18751e54cf5597311
AbdulSamiShafqatAli <abdulsamikhan890@gmail.com>
2021-12-02 14:35:46 +0500

    update color picker using hooks

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Colors from '../src/Color'
+import './App.css';
+
+function App() {
+  return (
+
+  
+    <div className="App">
+
+
+
+
+      
+
+      <Colors />
+
+    
+      
+
+
+    </div>
+  );
+}
+
+export default App;
